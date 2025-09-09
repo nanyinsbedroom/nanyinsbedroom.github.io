be21393c00708066ac4f60030f6fb273d4d629f1
Bqasta <bqastaa@gmail.com>
2025-09-09 04:18:47 -0800

    v2.1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,14 +15,14 @@ export const metadata: Metadata = {
     description: "Explore player statistics, crew rankings, and regional activity.",
     url: "https://nanyinsbedroom.github.io",
     siteName: "Player Dashboard",
-    images: [{ url: "https://raw.githubusercontent.com/nanyinsbedroom/nanyinsbedroom.github.io/main/public/og-image2png", width: 1200, height: 630, alt: "Dashboard Preview" }],
+    images: [{ url: "https://raw.githubusercontent.com/nanyinsbedroom/nanyinsbedroom.github.io/main/public/og-image.png", width: 1200, height: 630, alt: "Dashboard Preview" }],
     type: "website",
   },
   twitter: {
     card: "summary_large_image",
     title: "Player Data Dashboard",
     description: "Explore player statistics, crew rankings, and regional activity.",
-    images: ["https://raw.githubusercontent.com/nanyinsbedroom/nanyinsbedroom.github.io/main/public/og-image2.png"],
+    images: ["https://raw.githubusercontent.com/nanyinsbedroom/nanyinsbedroom.github.io/main/public/og-image.png"],
   },
 };
 
